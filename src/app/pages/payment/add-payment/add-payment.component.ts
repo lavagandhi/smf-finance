501c3cb46fa40ae0ca0ce6ad994d1ee6d9110fb7
gandhi.c <ghandhi@armaxindia.com>
2020-12-26 13:31:06 +0530

    payment add page correction

--- src/app/pages/payment/add-payment/add-payment.component.ts
+++ src/app/pages/payment/add-payment/add-payment.component.ts
@@ -7,7 +7,16 @@ import {
 } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
-
+interface Person {
+	id: string;
+	loanaccountno: string;
+	goldloanbranch: string;
+	micropinbranch: string;
+	existingcenter: string;
+	centergroup: string;
+	existinggroup: string;
+	centeraddress: string;
+  }
 @Component({
   selector: 'app-add-payment',
   templateUrl: './add-payment.component.html',
@@ -15,17 +24,73 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AddPaymentComponent implements OnInit {
 
+	isVisible = false;
+
+	listOfData: Person[] = [
+		{
+		  id: '1',
+		  loanaccountno: 'John Brown',
+		  goldloanbranch: 'New York',
+		  micropinbranch: 'New York',
+		  existingcenter: 'New York',
+		  centergroup: 'New York',
+		  existinggroup: 'New York',
+		  centeraddress: 'New York',
+		},
+		{
+		  id: '2',
+		  loanaccountno: 'Jim Green',
+		  goldloanbranch: 'London',
+		  micropinbranch: 'London',
+		  existingcenter: 'London',
+		  centergroup: 'London',
+		  existinggroup: 'London',
+		  centeraddress: 'London',
+		},
+		{
+		  id: '3',
+		  loanaccountno: 'Joe Black',
+		  goldloanbranch: 'Sidney',
+		  micropinbranch: 'Sidney',
+		  existingcenter: 'Sidney',
+		  centergroup: 'Sidney',
+		  existinggroup: 'Sidney',
+		  centeraddress: 'Sidney',
+		},
+	  ];
 
   title = 'Payment';
   btnName = 'Submit';
-  constructor(private fb: FormBuilder, ) { }
+
+  constructor(private fb: FormBuilder,) { }
   validateForm: FormGroup;
 
   ngOnInit() {
     this.validateForm = this.fb.group({
-			centerid: [null, [Validators.required]],
-			groupid: [null, [Validators.required]],     
-    });
+			typeofaccount: [null, [Validators.required]],
+			name: [null, [Validators.required]],
+			nameofthebank: [null, [Validators.required]],
+			branch: [null, [Validators.required]],
+      modeofoperation: [null, [Validators.required]],
+      accountno: [null, [Validators.required]],
+      ifsccode: [null, [Validators.required]],
+      
+		});
+  }
+
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
   }
 
   submitForm(form: FormGroup): void {
