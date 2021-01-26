c0b04fd6409c33d2216bf805e2a1ce6998a81269
lavagandhi <webriversgandhi@gmail.com>
2021-01-26 13:49:43 +0530

    print page design

--- src/app/pages/export/export.module.ts
+++ src/app/pages/export/export.module.ts
@@ -6,14 +6,15 @@ import { IconsProviderModule } from 'src/app/icons-provider.module';
 import { SharedModule } from 'src/app/shared';
 import { FormsModule } from '@angular/forms';
 import { ExportService } from 'src/app/pages/export/export.service';
-
+import {NgxPrintModule} from 'ngx-print';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     SharedModule,
     IconsProviderModule,
-    ExportRoutingModule
+    ExportRoutingModule,
+    NgxPrintModule
   ],
   declarations: [ExportFormComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
